1443cf8b58d0b9a7bbc18bc02ea04aeafffd1ff7
iluha5596 <pantik96@mail.ru>
2023-12-13 17:58:37 +0300

    change time.sleep

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ def test_add_to_cart_button_presence(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     # Временная задержка для наглядности
-    time.sleep(30)
+    time.sleep(10)
     # Проверка наличии кнопки
     buttonAddCart = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
     assert buttonAddCart, "Add to cart button is not present"
